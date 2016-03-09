3c3c14f98d7371d5acf01b5e95e4ae086e487104
Henrik Jonsson <me@hkjn.me>
2016-03-10 00:49:39 +0100

    Use TLS, verify signature

--- armv7l/build_image.sh
+++ armv7l/build_image.sh
@@ -3,16 +3,19 @@
 # Creates arch image for ARMv7l based on latest release from
 # http://archlinuxarm.org/os.
 #
+#
 set -euo pipefail
 
-SERVER=http://archlinuxarm.org/os
+SERVER=https://archlinuxarm.org/os
 BINARY=ArchLinuxARM-utilite-latest.tar.gz
 IMAGE=${IMAGE:-hkjn/armv7l-arch-base}
 SIG=${BINARY}.sig
+KEY=2BDBE6A6
 echo "Downloading $BINARY.."
 curl -fsSL "$SERVER/$BINARY" -o $BINARY
 echo "Downloading $SIG.."
 curl -fsSL "$SERVER/$SIG" -o $SIG
-# TODO(henrik): Verify (binary GPG?) .sig against static key fingerprint.
+gpg --keyserver pgp.mit.edu --recv $KEY
+gpg --verify ArchLinuxARM-utilite-latest.tar.gz.sig
 cat $BINARY | gunzip | docker import - $IMAGE
 echo "Built $IMAGE successfully."
