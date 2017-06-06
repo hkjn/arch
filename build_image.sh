9d38ad802be6c29cc2623f64c7ba6fd0679a6876
Henrik Jonsson <me@hkjn.me>
2017-06-06 20:59:30 +0200

    Tag images as hkjn/arch:x86_64

--- build_image.sh
+++ build_image.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #
-# Builds and pushes the docker image.
+# Build the hkjn/arch Docker images.
 #
-IMAGE=${IMAGE:-hkjn/$(uname -m)-arch}
 NO_PUSH=${NO_PUSH:-""}
-[ -e "archlinux.tar.xz" ] || {
+DOCKER_USER="hkjn"
+DOCKER_IMAGE="arch"
+ARCH="$(uname -m)"
+
+[[ -e "archlinux.tar.xz" ]] || {
 	echo "No archlinux.tar.xz. Run bootstrap.sh to create it." >&2
 	exit 1
 }
 
-# Copy over files we need to a new build context rather than having to
-# send all of the pwd to the Docker daemon.
-tmp="$(mktemp -d)"
-cp Dockerfile $tmp/
-cp archlinux.tar.xz $tmp/
+TAG="$DOCKER_USER/$DOCKER_IMAGE:$ARCH"
+BUILD_DIR="$(mktemp -d)"
+cp Dockerfile archlinux.tar.xz $BUILD_DIR/
 echo "Building $IMAGE.."
-docker build -t $IMAGE $tmp/ && [ "$NO_PUSH" ] || docker push $IMAGE
+docker build -t $IMAGE $BUILD_DIR/
+[[ "$NO_PUSH" ]] || docker push $TAG
